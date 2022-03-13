27686b60230f9d2c523c32e2339a6124a67f7042
paintedlbird7 <paintedl.bird@gmail.com>
2022-03-12 18:22:36 -0800

    gets data from firebase WIP

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import ItemComponent from './ItemComponent';
+import database from '@react-native-firebase/database';
 
-export default function App() {
+let itemsRef = database().ref('/task');
+
+export default function List() {
+  console.log(List);
+  const [itemsArray, setItemsArray] = React.useState([]);
+
+  React.useEffect(() => {
+    itemsRef.on('value', snapshot => {
+      console.log(data);
+      let data = snapshot.val();
+      const task = Object.values(data);
+      setItemsArray(task);
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      {/* <Flatlist /> */}
+      {itemsArray.length > 0 ? (
+        <ItemComponent task={itemsArray} />
+      ) : (
+        <Text>No items</Text>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ebebeb',
+  },
+});
